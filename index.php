949be1e97d3d7ea69345e77b53c3bac2425e175b
Cleve <gomescleve@gmail.com>
2015-12-25 14:14:52 -0800

    added the localdata save code

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <?php
 
-echo "this is just to test"; 
+
+$con = mysqli_connect('localhost','root','lifebuoy');
+define("APIKEY","cg123");
+
+if(!mysqli_connect_error())
+{
+	mysqli_select_db($con,"stormy");
+
+	if(isset($_REQUEST["apikey"]) && $_REQUEST["apikey"] == APIKEY)
+	$sql = "insert into localsensors
+			SET
+			temperature = '{$_REQUEST["temp"]}',
+			humidity = '{$_REQUEST["hum"]}',
+			pressure = '{$_REQUEST["psure"]}',
+			apikey = '{$_REQUEST["apikey"]}'
+			";
+
+
+	if(mysqli_query($con,$sql))
+	{
+		echo "success";
+		return 1;
+	}
+	mysqli_close($con);
+
+}
+
+
+echo "failed";
+return 0;
 
 
 ?>
