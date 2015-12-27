2cc59e9f488069e186124ac645f2b173bef769dd
Cleve <gomescleve@gmail.com>
2015-12-27 13:01:29 -0800

    test

--- rest.php
+++ rest.php
@@ -4,7 +4,6 @@ include_once "settings.php";
 $return_ary = [];
 $sql = "select * from localsensors order by id desc limit 0,1";
 
-//$timeCorrection = 4*60*60;
 
 $rst = mysqli_query($con,$sql);
 $row = mysqli_fetch_assoc($rst);
