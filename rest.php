78ff54d19916409292bbfbc022a215fc2d2f0bf0
Cleve <gomescleve@gmail.com>
2015-12-27 12:14:39 -0800

    test

--- rest.php
+++ rest.php
@@ -3,14 +3,17 @@ include_once "settings.php";
 
 $return_ary = [];
 $sql = "select * from localsensors order by id desc limit 0,1";
-
+echo $sql;
+return $sql;
+/*
 if($rst = mysqli_query($con,$sql))
 	{
 		 $return_ary[$rst["id"] = [ "t"=>$rst["temperature"],
 									"p"=>$rst["pressure"],
 									"h"=>$rst["humidity"]];			
 	}
-	echo json_decode(return_ary);	
-	echo "cg";	
+	echo json_decode(return_ary);		
 	return;
+	
+	*/
 ?>
