b92e5fb0eae7eddc5d78d61ddfd269bef651e443
Cleve <gomescleve@gmail.com>
2015-12-27 12:34:43 -0800

    test

--- rest.php
+++ rest.php
@@ -8,13 +8,15 @@ $sql = "select * from localsensors order by id desc limit 0,1";
 if($rst = mysqli_query($con,$sql))
 	{
 	
-		// $return_ary[$rst["current_timestamp"]] = $rst["temperature"]["t"=>$rst["temperature"],"p"=>$rst["pressure"],"h"=>$rst["humidity"]];			
+		$row = mysqli_fetch_assoc($row);
+		
+		$return_ary[$row["current_timestamp"]] = $row["temperature"]["t"=>$row["temperature"],"p"=>$row["pressure"],"h"=>$rst["humidity"]];			
 	
 	}
 	
 
 
-	echo json_encode(return_ary);		
+	echo json_encode($return_ary);		
 	return;
 
 	
