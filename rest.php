864985cec760f03d68e36ab7826ff1ee1b6da87f
Cleve <gomescleve@gmail.com>
2015-12-27 13:01:45 -0800

    test

--- rest.php
+++ rest.php
@@ -5,6 +5,7 @@ $return_ary = [];
 $sql = "select * from localsensors order by id desc limit 0,1";
 
 
+
 $rst = mysqli_query($con,$sql);
 $row = mysqli_fetch_assoc($rst);
 $return_ary[$row["created_timestamp"]] = ["t"=>$row["temperature"],"p"=>$row["pressure"],"h"=>$row["humidity"]];
