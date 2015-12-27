9aa1050d259bce877bc46b9d52ab1da373e79a35
Cleve <gomescleve@gmail.com>
2015-12-27 13:00:27 -0800

    test

--- rest.php
+++ rest.php
@@ -4,11 +4,11 @@ include_once "settings.php";
 $return_ary = [];
 $sql = "select * from localsensors order by id desc limit 0,1";
 
-$timeCorrection = 4*60*60;
+//$timeCorrection = 4*60*60;
 
 $rst = mysqli_query($con,$sql);
 $row = mysqli_fetch_assoc($rst);
-$return_ary[$row["created_timestamp"]+$timeCorrection] = ["t"=>$row["temperature"],"p"=>$row["pressure"],"h"=>$row["humidity"]];
+$return_ary[$row["created_timestamp"]] = ["t"=>$row["temperature"],"p"=>$row["pressure"],"h"=>$row["humidity"]];
 	
 	
 	
